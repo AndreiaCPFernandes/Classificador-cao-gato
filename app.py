f4a58426c91d880cab8bbfd75a967088e971d45a
AndreiaCPFernandes <andreiacpatricio@gmail.com>
2025-06-17 18:08:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask, request, render_template
-from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import numpy as np
 import os
+import pickle
 
 app = Flask(__name__)
-modelo = modelo_de_carga('templates/meu_modelo_gato_cachorro.pkl')
 
+# Carregue seu modelo pickle
+with open('templates/meu_modelo_gato_cachorro.pkl', 'rb') as f:
+    model = pickle.load(f)
 
 def prepare_image(img_path):
-    img = image.load_img(img_path, target_size=(150, 150))  # Ajuste o tamanho se necessário
+    img = image.load_img(img_path, target_size=(150, 150))  # ajuste o tamanho se necessário
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
     img_array = img_array / 255.0
@@ -24,8 +26,10 @@ def index():
 
         img_array = prepare_image(filepath)
         prediction = model.predict(img_array)[0][0]
+
         result = "Gato" if prediction < 0.5 else "Cachorro"
         return render_template("index.html", prediction=result)
+    
     return render_template("index.html", prediction=None)
 
 if __name__ == "__main__":
